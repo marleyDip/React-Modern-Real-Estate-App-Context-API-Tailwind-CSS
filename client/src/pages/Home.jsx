3c0a17d8feb174478cf16302aa3a60311b1bb9cd
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-26 10:26:08 +0600

    Home All Section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import About from "../Components/About";
+import Cta from "../Components/Cta";
+import Faq from "../Components/Faq";
+import FeaturedProperties from "../Components/FeaturedProperties";
+import Hero from "../Components/Hero";
+import Testimonial from "../Components/Testimonial";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-gradient-to-r from-[#fffbee] to-white">
+      <Hero />
+      <About />
+      <FeaturedProperties />
+      <Faq />
+      <Cta />
+      <Testimonial />
+    </div>
+  );
 };
 
 export default Home;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,5 @@
 const Listing = () => {
-  return <div>Listing</div>;
+  return <div className="h-[200px] bg-green-50">Listing</div>;
 };
 
 export default Listing;
