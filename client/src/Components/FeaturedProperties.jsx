13596c74080fb5fdbde36668b20bb9f2d4a495d0
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-27 00:50:33 +0600

    Featured Product design & complete

--- client/src/Components/FeaturedProperties.jsx
+++ client/src/Components/FeaturedProperties.jsx
@@ -7,50 +7,62 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 // import required modules
 import { Autoplay } from "swiper/modules";
+import { useAppContext } from "../context/AppsContext";
+import Item from "./Item";
 
 const FeaturedProperties = () => {
+  const { properties } = useAppContext();
+  console.log(properties);
+
   return (
-    <section>
-      <div>
-        <span className="medium-18">Your New Home Awaits!</span>
-        <h2>Discover Your Place Here</h2>
+    <section className="max-padd-container py-16 xl:py-22">
+      <span className="medium-18">Your New Home Awaits!</span>
+      <h2 className="h2">Discover Your Place Here</h2>
 
-        <div>
-          <h5>
-            <span>Displaying 1-9</span> from 3k listings
-          </h5>
-          <Link to={"/listing"} onClick={() => scrollTo(0, 0)}>
-            <img src={assets.sliders} alt="sliderIcon" />
-          </Link>
-        </div>
+      <div className="flexBetween mt-8 mb-6">
+        <h5 className="font-medium">
+          <span className="font-bold">Displaying 1-9</span> from 3k listings
+        </h5>
 
-        {/* container */}
-        <Swiper
-          autoplay={{
-            delay: 3500,
-            disableOnInteraction: false,
-          }}
-          breakpoints={{
-            600: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
-            1124: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-            1300: {
-              slidesPerView: 4,
-              spaceBetween: 30,
-            },
-          }}
-          modules={[Autoplay]}
-          className="h-[488px] md:h-[533px] xl:h-[422px] mt-5"
+        <Link
+          to={"/listing"}
+          onClick={() => scrollTo(0, 0)}
+          className="bg-secondary/10 ring-1 ring-slate-900/15 text-2xl rounded-md hover:shadow-md hover:-translate-x-1 duration-200 ease-out p-2 flexCenter"
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-        </Swiper>
-        {/* container */}
+          <img src={assets.sliders} alt="sliderIcon" />
+        </Link>
       </div>
+
+      {/* container */}
+      <Swiper
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          600: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1124: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          /* 1300: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          }, */
+        }}
+        modules={[Autoplay]}
+        className="h-[488px] md:h-[533px] mt-5"
+      >
+        {properties.slice(0, 6).map((property) => (
+          <SwiperSlide key={property.title}>
+            <Item property={property} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* container */}
     </section>
   );
 };
---
+++ client/src/Components/Item.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../assets/data";
+
+const Item = ({ property }) => {
+  return (
+    <Link
+      to={"/listing" + property._id}
+      className="group block rounded-lg bg-white ring-1 ring-slate-900/5 shadow-md hover:shadow-lg"
+    >
+      {/* image */}
+      <div className="relative overflow-hidden">
+        <img
+          src={property.images[0]}
+          alt={property.title}
+          className="h-[16rem] w-full aspect-square object-cover rounded-t-xl group-hover:scale-105 transition-all duration-500 ease-in-out"
+        />
+
+        <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out flex gap-1 items-start p-2">
+          {property.amenities.map((item, index) => (
+            <span
+              key={index}
+              className="px-3 py-1 text-xs font-medium bg-white/90 text-gray-700 backdrop-blur-sm rounded-full"
+            >
+              {item}
+            </span>
+          ))}
+
+          <span className="absolute bottom-2 left-2 px-3 py-1 text-xs font-medium bg-white/90 text-gray-700 backdrop-blur-sm rounded-full">
+            {property.city}
+          </span>
+        </div>
+
+        {/* <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+          <span className="absolute top-0 bottom-4 px-3 py-1 text-xs font-medium bg-white/90 text-gray-700 backdrop-blur-sm rounded-full">
+            {property.amenities.join(", ")}
+          </span>
+        </div> */}
+      </div>
+      {/* image */}
+
+      {/* info */}
+      <div className="p-3">
+        <div className="flexBetween">
+          <h5 className="bold-16 my-2">{property.propertyType}</h5>
+
+          <div className="bold-15 text-amber-600">
+            ৳{property.price.sale} | ৳{property.price.rent}.00{" "}
+            <span className="text-sm">/night</span>
+          </div>
+        </div>
+
+        <h4 className="h4 line-clamp-1">{property.title}</h4>
+
+        <div className="flex py-4 space-x-4 divide-x divide-slate-900/50">
+          <p className="flexCenter gap-x-2 pr-4 font-medium">
+            <img src={assets.bed} alt="facilitiesIcon" width={21} />
+            {property.facilities.bedrooms}
+          </p>
+
+          <p className="flexCenter gap-x-2 pr-4 font-medium">
+            <img src={assets.bath} alt="facilitiesIcon" width={21} />
+            {property.facilities.bathrooms}
+          </p>
+
+          <p className="flexCenter gap-x-2 pr-4 font-medium">
+            <img src={assets.car} alt="facilitiesIcon" width={21} />
+            {property.facilities.garages}
+          </p>
+
+          <p className="flexCenter gap-x-2 pr-4 font-medium">
+            <img src={assets.ruler} alt="facilitiesIcon" width={21} />
+            {property.area}
+          </p>
+        </div>
+
+        <p className="pt-2 mb-4 line-clamp-2 font-medium text-justify text-neutral-700">
+          {property.description}
+        </p>
+      </div>
+      {/* info */}
+    </Link>
+  );
+};
+
+export default Item;
