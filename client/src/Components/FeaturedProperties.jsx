3334f6065c20cb37c9be01858d1e730e4600e6d1
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-26 14:09:15 +0600

    swiper js install

--- client/src/Components/FeaturedProperties.jsx
+++ client/src/Components/FeaturedProperties.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../assets/data";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+// import required modules
+import { Autoplay } from "swiper/modules";
 
 const FeaturedProperties = () => {
-  return <div>FeaturedProperties</div>;
+  return (
+    <section>
+      <div>
+        <span className="medium-18">Your New Home Awaits!</span>
+        <h2>Discover Your Place Here</h2>
+
+        <div>
+          <h5>
+            <span>Displaying 1-9</span> from 3k listings
+          </h5>
+          <Link to={"/listing"} onClick={() => scrollTo(0, 0)}>
+            <img src={assets.sliders} alt="sliderIcon" />
+          </Link>
+        </div>
+
+        {/* container */}
+        <Swiper
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1124: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            1300: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+          modules={[Autoplay]}
+          className="h-[488px] md:h-[533px] xl:h-[422px] mt-5"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+        </Swiper>
+        {/* container */}
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedProperties;
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProperties } from "../assets/data";
+
+const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const [properties, setProperties] = useState([]);
+
+  const navigate = useNavigate();
+
+  const getProperties = () => {
+    setProperties(dummyProperties);
+  };
+
+  useEffect(() => {
+    getProperties;
+  }, []);
+
+  const value = {
+    navigate,
+    properties,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
