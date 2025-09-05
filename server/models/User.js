da857ced4f7c379e3dfd81d7cfdb6ba2f48afede
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-09-05 15:37:23 +0600

    update

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
 
     username: { type: String, required: true },
 
-    email: { type: String, required: true },
+    email: { type: String, default: null },
 
     image: { type: String, default: "" }, // safer if user has no profile image
 
--- server/server.js
+++ server/server.js
@@ -15,27 +15,27 @@ app.use(cors()); // Enable Cross-Origin Resource Sharing for all routes
 app.use(express.json()); // Keep JSON parsing for your normal routes
 app.use(clerkMiddleware());
 
-/* // API to listen clerk webhooks
-app.use("/api/clerk", clerkWebhooks); */
+// API to listen clerk webhooks
+app.use("/api/clerk", clerkWebhooks);
 
 // 👇 Webhook route using raw body
-app.post(
-  "/api/clerk",
-  bodyParser.raw({ type: "application/json" }),
-  (req, res, next) => {
-    try {
-      // Parse raw buffer to JSON after verification
-      req.body = JSON.parse(req.body.toString());
-      next();
-    } catch (err) {
-      console.error("❌ Failed to parse webhook body:", err);
-      return res
-        .status(400)
-        .json({ message: "Invalid JSON body", success: false });
-    }
-  },
-  clerkWebhooks
-);
+// app.post(
+//   "/api/clerk",
+//   bodyParser.raw({ type: "application/json" }),
+//   (req, res, next) => {
+//     try {
+//       // Parse raw buffer to JSON after verification
+//       req.body = JSON.parse(req.body.toString());
+//       next();
+//     } catch (err) {
+//       console.error("❌ Failed to parse webhook body:", err);
+//       return res
+//         .status(400)
+//         .json({ message: "Invalid JSON body", success: false });
+//     }
+//   },
+//   clerkWebhooks
+// );
 
 //Route Endpoint to check server / API status
 app.get("/", (req, res) => {
